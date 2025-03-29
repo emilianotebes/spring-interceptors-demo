06723cd02212b3b053972a62efe99c6542359021
emiliano <emilianotebes@gmail.com>
2025-03-29 11:06:14 -0300

    Initialize repo

---
+++ src/test/kotlin/com/etsoft/interceptors/InterceptorsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.etsoft.interceptors
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InterceptorsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
