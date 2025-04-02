59978d8ec8ad160529ed9232530b8e2b33602a64
emiliano <emilianotebes@gmail.com>
2025-04-02 00:46:22 -0300

    Add comments

--- src/main/kotlin/com/etsoft/interceptors/LoggingFilter.kt
+++ src/main/kotlin/com/etsoft/interceptors/LoggingFilter.kt
@@ -16,18 +16,28 @@ class LoggingFilter : Filter {
         res: ServletResponse,
         filterChain: FilterChain
     ) {
+        // Tenemos que castear los requests
         val castedRequest = req as HttpServletRequest
         val castedResponse = res as HttpServletResponse
+
+        // Envolvemos los request en nuestros wrappers
         val wrappedRequest = ContentCachingRequestWrapper(castedRequest)
         val wrappedResponse = ContentCachingResponseWrapper(castedResponse)
 
         try {
+            // Invocamos al resto de las operaciones para asegurarnos de que tanto el request como el response se lean y escriban
+            // Notar que mandamos los wrappers, y no los objetos originales
             filterChain.doFilter(wrappedRequest, wrappedResponse)
         } finally {
+            // Una vez ejecutado filterChain, tenemos nuestro objetos cargados con la info
             val contentAsString = wrappedRequest.contentAsString
             val responseAsString = String(wrappedResponse.contentAsByteArray)
 
+            // Obligatoriamente tenemos que hacer esto, porque la conexión está establecida con el response original
+            // y nuestro filter chain escribió en una copia, si no ponemos esta línea el body del response será vacío
             wrappedResponse.copyBodyToResponse()
+
+            // Podemos acceder a los valores las veces que querramos
             println("Request: $contentAsString")
             println("Response: $responseAsString")
 
